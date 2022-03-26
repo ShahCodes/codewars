11526b88482492a7a37de5245d8891d2a599b20c
Mahavir Shah <mahavirshah2002@gmail.com>
2022-03-25 23:58:43 -0700

    Coding challenge and solution for 03-25-22 coding challenge

---
+++ 03-25-2022/app.js
@@ -0,0 +1,21 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/583710ccaa6717322c000105
+
+// Question:
+
+/* 
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+// Solution: 
+
+
+function simpleMultiplication(number) {
+  if(number % 2 == 0) {
+    return number * 8
+  }else {
+    return number * 9
+  }
+}
+
+simpleMultiplication(2)
+
