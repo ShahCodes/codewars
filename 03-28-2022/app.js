db0224b3b38145d98e23a5600ccc4fc24ed454b3
Mahavir Shah <mahavirshah2002@gmail.com>
2022-03-28 08:37:25 -0700

    Coding challenge and solution for 03-28-22 coding challenge

---
+++ 03-28-2022/app.js
@@ -0,0 +1,21 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// Question:
+
+/* 
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+// Solution: 
+
+
+function booleanToString(b){
+  let trueOrFalse = b
+  let text = trueOrFalse.toString()
+  
+  return text
+}
+
+booleanToString(true)
