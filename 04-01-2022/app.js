8904318db2b7d046366572f42bd408ca8847f848
Mahavir Shah <mahavirshah2002@gmail.com>
2022-04-01 09:15:17 -0700

    Coding challenge and solution for 04-01-22 coding challenge

---
+++ 04-01-2022/app.js
@@ -0,0 +1,19 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+// Question:
+
+/* 
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+*/
+
+// Solution: 
+
+
+function multiply(a, b){
+  return a * b
+}
+multiply(999, 1)
