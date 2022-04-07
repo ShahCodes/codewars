a79449d5d899528f7d8776e275cd5aa86623075e
Mahavir Shah <mahavirshah2002@gmail.com>
2022-04-07 10:04:35 -0700

    Coding challenge and solution for 04-07-22 coding challenge

---
+++ 04-07-2022/app.js
@@ -0,0 +1,20 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+// Question:
+
+/* 
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+// Solution: 
+
+
+let websites = []
+websites.push('codewars')
+
+websites // [ 'codewars' ]
