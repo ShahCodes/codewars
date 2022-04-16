3f4965d63493786b4ffab83935d7555d7a151dc6
Mahavir Shah <mahavirshah2002@gmail.com>
2022-04-16 10:19:42 -0700

    Coding challenge and solution for 04-16-22 coding challenge

---
+++ 04-16-2022/app.js
@@ -0,0 +1,24 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// Question:
+
+/* 
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+
+// Solution: 
+
+
+function numberToString(num) {
+
+    let convert = String(num)
+    return convert
+}
+
+numberToString(999)
