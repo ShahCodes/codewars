9951c9a4929666a109221acd6a8f9519910b7f2b
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-05 21:13:11 -0700

    Coding challenge and solution for 05-05-22 coding challenge

---
+++ 05-05-2022/app.js
@@ -0,0 +1,22 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// Question:
+
+/* 
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// Solution: 
+
+
+function opposite(number) {
+  return number * -1
+}
+
+opposite(-999)
