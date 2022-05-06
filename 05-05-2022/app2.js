4d744b8aebdc68c3782739f18a44601e91f3308b
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-05 21:18:30 -0700

    2nd Coding challenge and solution for 05-05-22 coding challenge

---
+++ 05-05-2022/app2.js
@@ -0,0 +1,16 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+// Question:
+
+/* 
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+
+// Solution: 
+
+
+function removeChar(str){
+  return str.slice(1, -1)
+};
+
+removeChar('99999')
