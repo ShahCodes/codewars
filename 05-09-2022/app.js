3cfbc67d515ed1625b97231af4128b6bee12e0c1
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-09 09:52:18 -0700

    Coding challenge and solution for 05-09-22 coding challenge

---
+++ 05-09-2022/app.js
@@ -0,0 +1,20 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+// Question:
+
+/* 
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+// Solution: 
+
+
+function maps(x){
+  return x.map(n => n * 2)
+}
+
+maps([1,2,3,4,5,6,7,8,9,])
