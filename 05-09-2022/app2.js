e615e65a85ffa13d64d70d50ad19f51b98473838
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-09 09:56:30 -0700

    2nd Coding challenge and solution for 05-09-22 coding challenge

---
+++ 05-09-2022/app2.js
@@ -0,0 +1,16 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// Question:
+
+/* 
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// Solution: 
+
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+doubleInteger(12)
