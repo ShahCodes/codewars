cbdba28c5932b6986591f1724f1a31009def792d
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-10 20:56:12 -0700

    2nd Coding challenge and solution for 05-10-22 coding challenge

---
+++ 05-10-2022/app2.js
@@ -0,0 +1,15 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+// Question:
+
+/* 
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+// Solution: 
+
+
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
+
