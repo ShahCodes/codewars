b1796d343ac2bf09f2899f8e72b896125cb83b9b
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-16 10:07:51 -0700

    Coding challenge and solution for 05-16-22 coding challenge

---
+++ 05-16-2022/app.js
@@ -0,0 +1,14 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+// Question:
+
+/* 
+Write a function which converts the input string to uppercase.
+*/
+
+// Solution: 
+
+
+function makeUpperCase(str) {
+  return str.toUpperCase()
+}
