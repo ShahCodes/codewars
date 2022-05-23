bf704a7a2e9e9457c1d766f516e4b22e3fe83fed
Mahavir Shah <mahavirshah2002@gmail.com>
2022-05-23 15:21:16 -0700

    Coding challenge and solution for 05-23-22 coding challenge

---
+++ 05-23-2022/app.js
@@ -0,0 +1,23 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// Question:
+
+/* 
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+*/
+
+// Solution: 
+
+function hero(bullets, dragons){
+  let bulletsNeeded = dragons * 2
+  
+  if(bullets >= bulletsNeeded) {
+    return true
+  }else {
+    return false
+  }
+}
+
+hero(10, 5)
