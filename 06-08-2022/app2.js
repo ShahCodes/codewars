e35a458bfecc7aa3a62991c43839be13a242afb2
Mahavir Shah <mahavirshah2002@gmail.com>
2022-06-08 21:57:33 -0700

    2nd Coding challenge and solution for 06-08-22 coding challenge

---
+++ 06-08-2022/app2.js
@@ -0,0 +1,14 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+// Question:
+
+/* 
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+// Solution: 
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '');
+}
+removeExclamationMarks('Hi! Hello!')
