169feca7c1783484f4f9a7e5f807050b159336f9
Mahavir Shah <mahavirshah2002@gmail.com>
2022-06-17 14:18:15 -0700

    Coding challenge and solution for 06-17-22 coding challenge

---
+++ 06-17-2022/app.js
@@ -0,0 +1,14 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba
+
+// Question:
+
+/* 
+Return the type of the sum of the two arguments
+*/
+
+// Solution: 
+
+function typeOfSum(a, b) {
+  let values = a + b
+  return typeof values
+}
