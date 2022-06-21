5162f9f06e5971b3fa21b63d6fd9f6d7140bb615
Mahavir Shah <mahavirshah2002@gmail.com>
2022-06-21 11:54:25 -0700

    Coding challenge and solution for 06-21-22 coding challenge

---
+++ 06-21-2022/app.js
@@ -0,0 +1,21 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/56e2f59fb2ed128081001328
+
+// Question:
+
+/* 
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+*/
+
+// Solution: 
+
+function printArray(array){
+  return array.join(',')
+}
