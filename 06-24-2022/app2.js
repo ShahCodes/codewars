3dee3b2afba2650929c122e73f1e44ea7d18e482
Mahavir Shah <mahavirshah2002@gmail.com>
2022-06-24 12:40:13 -0700

    2nd Coding challenge and solution for 06-24-22 coding challenge

---
+++ 06-24-2022/app2.js
@@ -0,0 +1,24 @@
+// Link To Kata: 8ku https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
+// Question:
+
+/* 
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+// Solution: 
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let findSmallInt = args.sort((a,b)=> a - b)
+    return findSmallInt[0]
+  }
+}
+let sif = new SmallestIntegerFinder()
+sif.findSmallestInt([78,56,232,12,8])
